6dac44c962465b2f6451862923629553c45da720
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-03-12 23:49:08 -0800

    added new view test for MyAccount

---
+++ AnkiDroid/src/androidTest/java/com/sxmp/apprentice/MyAccountTest.java
@@ -0,0 +1,47 @@
+package com.sxmp.apprentice;
+
+import com.ichi2.anki.MyAccount;
+import com.ichi2.anki.NavigationDrawerActivity;
+import com.ichi2.anki.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+public class MyAccountTest {
+
+    //login_button
+    private ActivityTestRule<MyAccount> activityRule =
+            new ActivityTestRule<>(MyAccount.class, false, false);
+
+    private GrantPermissionRule grantRule =
+            GrantPermissionRule.grant(WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE);
+
+    @Rule
+    public RuleChain ruleChain = RuleChain.outerRule(grantRule).around(activityRule);
+
+    @Before
+    public void setup() {
+        activityRule.launchActivity(null);
+    }
+
+    @Test
+    public void test_isLayoutInView() {
+
+        onView(withId(R.id.login_button)).check(matches(isDisplayed()));
+
+    }
+
+}
