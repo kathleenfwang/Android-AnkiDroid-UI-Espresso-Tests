8e7474438788a30c86bc3330dc11b42f3af33a43
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-03-13 00:17:01 -0800

    add test to check if clicking login button will not redirect if username/password fields are blank

--- AnkiDroid/src/androidTest/java/com/sxmp/apprentice/MyAccountTest.java
+++ AnkiDroid/src/androidTest/java/com/sxmp/apprentice/MyAccountTest.java
@@ -15,9 +15,15 @@ import androidx.test.rule.GrantPermissionRule;
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 public class MyAccountTest {
@@ -38,10 +44,19 @@ public class MyAccountTest {
     }
 
     @Test
-    public void test_isLayoutInView() {
-
+    public void test_isLoginButtonDisplayed() {
         onView(withId(R.id.login_button)).check(matches(isDisplayed()));
-
     }
-
+    @Test
+    public void test_loginButtonDoesNotRedirectWhenNoUser() {
+        // first clear the password and username forms
+        onView(withId(R.id.password)).perform(clearText());
+        onView(withId(R.id.username)).perform(clearText());
+        // click login button
+        onView(withId(R.id.login_button)).perform(click());
+        // can also check if the root layout(Login Activity) is still being displayed
+//        onView(withId(R.id.root_layout)).check(matches(isDisplayed()));
+        // username logged in form should not exist
+        onView(withId(R.id.username_logged_in)).check(doesNotExist());
+    }
 }
