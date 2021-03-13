65ca00f2343e87a0a55cf69718550553bb2e2c34
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-03-12 22:49:56 -0800

    Add test case in 'SampleDeckPicker' for onViewCheck

--- AnkiDroid/src/androidTest/java/com/sxmp/apprentice/SampleDeckPickerTest.java
+++ AnkiDroid/src/androidTest/java/com/sxmp/apprentice/SampleDeckPickerTest.java
@@ -1,17 +1,25 @@
 package com.sxmp.apprentice;
 
 import com.ichi2.anki.DeckPicker;
+import com.ichi2.anki.R;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.rule.GrantPermissionRule;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 
 public class SampleDeckPickerTest {
 
@@ -30,7 +38,14 @@ public class SampleDeckPickerTest {
     }
 
     @Test
-    public void oneOfYourTests() {
+    public void test_isDeckPickerInView() {
+        onView(withId(R.id.deckpicker_view)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void test_isTextCorrect() {
+        onView(withText(R.string.no_cards_placeholder_title)).check(matches(not(isDisplayed())));
 
     }
+
 }
