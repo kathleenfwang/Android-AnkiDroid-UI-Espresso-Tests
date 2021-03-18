667dce3c4b05603930d8a1922e0256d2e9faa3e7
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-03-17 19:42:53 -0700

    test completed for spinner dropdown displaying 'Question'

--- AnkiDroid/src/androidTest/java/com/sxmp/apprentice/CardBrowserTest.java
+++ AnkiDroid/src/androidTest/java/com/sxmp/apprentice/CardBrowserTest.java
@@ -14,12 +14,19 @@ import androidx.test.rule.GrantPermissionRule;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.hasFocus;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
@@ -48,10 +55,20 @@ public class CardBrowserTest {
     onView(withId(R.id.search_src_text)).check(matches(hasFocus()));
     }
     @Test
-    public void test_isFirstCardHighlighted() {
-        // check if first item is displayed
-        
-        // check if it has background
+    public void test_isSpinnerDisplayingQuestion() {
+        // check if the spinner for column1 is displayed
+        onView(withId(R.id.browser_column1_spinner)).check(matches(isDisplayed()))
+                // click the spinner to get dropdown
+                .perform(click());
+        // check if all of the spinner dropdown data contains strings
+        onData(allOf(is(instanceOf(String.class)),
+                // check if there is a "Question" tab in spinner dropdown
+                is("Question")))
+                // click the "Question" tab
+                .perform(click());
+        // check if the text box now displays "Question"
+        onView(withText("Question")).check(matches(isDisplayed()));
+        // check the spinner text now displaying "Question"
+        onView(withId(R.id.browser_column1_spinner)).check(matches(withSpinnerText(containsString("Question"))));
     }
-
 }
